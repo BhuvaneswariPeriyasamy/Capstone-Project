4236fab80ccbd4d0277fe1435088af9ded471d79
JogeeshwaranV <jogeeshwaran@gmail.com>
2024-10-07 18:33:15 -0400

    API - Get User by ID and Delete User

--- API/routes/userRoutes.js
+++ API/routes/userRoutes.js
@@ -39,6 +39,31 @@ router.post('/create', async (req, res) => {
       res.status(500).json({ message: err.message });
     }
   });
+
+
+  // Get a user by ID
+  router.get('/:id', async (req, res) => {
+    try {
+      const user = await User.findOne({ userId: req.params.id });
+      if (!user) return res.status(404).json({ message: 'User not found' });
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+
+
+   // Delete a user by ID
+   router.delete('/delete/:id', async (req, res) => {
+    try {
+      const deletedUser = await User.findOneAndDelete({ userId: req.params.id });
+      if (!deletedUser) return res.status(404).json({ message: 'User not found' });
+      res.status(200).json({ message: 'User deleted successfully' });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+
   
   router.get('/', async (req, res) => {
     try {
